55433389b836d1f58b8d2c55831eb05683c158b6
Aman Dwivedi <aman.dwivedi@ofbusiness.in>
2020-04-04 16:20:19 +0530

    MoveZeroes - optimized code

--- problems/MoveZeroes.java
+++ problems/MoveZeroes.java
@@ -1,27 +1,25 @@
+// https://www.geeksforgeeks.org/move-zeroes-end-array-set-2-using-single-traversal/
 
 public class MoveZeroes {
     public static void main(String args[]) {
         int[] nums = new int[] { 0, 1, 0, 3, 12 };
         int[] result = moveZeroes(nums);
         for (int num : result) {
-            System.out.print(num);
+            System.out.print(num + " ");
         }
     }
 
     private static int[] moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int j = i + 1;
-            if (nums[i] == 0 && j < nums.length) {
-                while (j < nums.length && nums[j] == 0) {
-                    j++;
-                }
-                if (j < nums.length && nums[j] != 0) {
-                    // swap
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+        int nonzeroNumIndex = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[nonzeroNumIndex] = nums[i];
+                nonzeroNumIndex++;
             }
+        }
+        
+        for(int i = nonzeroNumIndex; i < nums.length; i++) {
+            nums[i] = 0;
         }
         return nums;
     }
